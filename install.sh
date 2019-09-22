618427fd9ebd2fc8ba394e284ac95c28aa103a6b
Stian Seglem Bjåland <31239242+Chrystallic@users.noreply.github.com>
2019-09-22 11:54:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
 # Installs make and git before all other packages
-pacman -S make git
+pacman -S make git curl
+
+# Install audio-drivers
+sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils
+
+# Install WM(bspwm), keyboardshortcuts, system monitor, terminal
+sudo pacman -S bspwm sxhkd conky htop dmenu xorg xorg-server xorg-xprops xorg-xinit rxvt-unicode
+
+# Download and create bspwm and sxhkd configfiles
+mkdir ~/.config 
+mkdir ~/.config/bspwm
+curl https://raw.githubusercontent.com/baskerville/bspwm/master/examples/bspwmrc --output bspwm/bspwmrc
+mkdir ~/.config/sxhkd
+curl https://raw.githubusercontent.com/baskerville/bspwm/master/examples/sxhkdrc --output sxhkd/sxhkdrc
+
+# Download program such as file managers unpackagers etc,
+sudo pacman -S tumbler xbindkeys binutils dunst xclip zathura-pdf-mupdf neovim flashplugin thunar ranger raw-thumbnailer unzip unrar compton file-roller clang scrot xf86-input-synaptics
+
+# Install yay to get access to the AUR
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd .. && rm -r yay/
+
+# Install AUR packages
+yay -S lightdm-gtk-greeter-settings feh neofetch polybar rofi lxappearance numix-icon-theme-git siji-git ttf-unifont ttf-font-awesome ttf-anonymous-pro noto-fonts 
+
+
+
+# Change default shell to Z-shell
+sudo pacman -S zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+chsh -s /usr/bin/zsh
+
+
+
 
 # Install fonts and programs
-sudo pacman -S  bspwm \
-                compton \
                 dash \ 
                 diff-so-fancy \
-                dunst \
-                exa \
-                feh \
                 libnotify \
                 maim \
                 mpd \
                 mpv \ 
-                neofetch \ 
-                neovim \
-                nnn \
-                noto-fonts \
-                noto-fonts-cjk \
                 ncmpcpp \
-                pulseaudio-alsa \
-                rofi \ 
-                rxvt-unicode \
-                sxhkd \
                 ttf-croscore \
                 ttf-fantasque-sans-mono \
                 ttf-linux-libertine \ 
-                vifm \
-                xclip \
-                xorg-server \
-                xorg-xinit \
-                xorg-xprop \
-                zathura-pdf-mupdf \
-                zsh \
-                zsh-completions \
-                zsh-autosuggestions \
-                zsh-syntax-highlighting \
                 ttf-joypixels \
                 
 # Generate symlinks
